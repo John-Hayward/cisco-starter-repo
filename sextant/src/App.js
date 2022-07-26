b6d90d377d4a83154483e09e61da048d34d1362e
John Hayward <haywardjohn11@gmail.com>
2022-07-26 11:59:59 +0100

    Added framework for a website title and Exhibits
    This added 2 new components to the website, Banner and Exhibit.
    The exhibit can inherit any component to be displayed.

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+class Banner extends React.Component{
+  render() {
+    const title = "Sextant User Info";
+
+    return( 
+      <div className="Title">
+        {title}
+      </div>
+    )
+  }
+}
+
+function Exhibit(props) {
+  return (
+    <div className="exhibit">
+      <h1 className="Exhibit-Title">
+        {props.title}
+      </h1>
+      <div className="components">
+        {props.component}
+      </div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Banner">
+        <Banner />
+      </div>
+
+      <div className="exhibit 1">
+        <Exhibit title={"Exhibit title"} component={"Exhibit component"}/>
+      </div>
+
+      <div className="exhibit 2">
+        <Exhibit title={"Exhibit 2 title"} component={"Exhibit 2 component"}/>
+      </div>
     </div>
   );
 }
